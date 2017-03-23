7e9bbe557378d32a0afb2a9413c849fdf462a985
Cara4lyfe <jsun39@hawk.iit.edu>
2017-03-22 23:03:38 -0500

    deleting figure

--- js/script-min.js
+++ js/script-min.js
@@ -2,4 +2,6 @@
     return $("img").each(function(){var a=$(this).data("alt");
         t.push(a)}),t},a=t(),r=[];$.each(a,function(t){r[t]=new Image,r[t].src=a[t]}),$("figure").on("click",function()
 {var t=$(this),a=t.index(),r=t.children("img"),i=r.attr("src"),n=r.attr("data-alt"),c=n.split(".");
-    "gif"===c[1]?r.attr("src",r.data("alt")).attr("data-alt",i):r.attr("src",n).attr("data-alt",r.data("alt")),t.toggleClass("play")})}(jQuery);
+    "gif"===c[1]?r.attr("src",r.data("alt")).attr("data-alt",i):r.attr("src",n).attr("data-alt",r.data("alt")),t.toggleClass("play")})
+    
+}(jQuery);
